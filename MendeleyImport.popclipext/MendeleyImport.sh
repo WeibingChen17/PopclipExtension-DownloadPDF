8c5686d5f22b6e589764f01bad19d867ce5a3bb1
Weibing Chen <weibing.chen.xy@gmail.com>
2017-03-30 22:32:29 -0500

    Minor bug fix

--- MendeleyImport.popclipext/MendeleyImport.sh
+++ MendeleyImport.popclipext/MendeleyImport.sh
@@ -1,17 +1,20 @@
 watchFolder=${POPCLIP_OPTION_MENDELEYWATCHEDFOLDER%/}
 downloadFile=${POPCLIP_TEXT##*/}
 downloadFile="${downloadFile%.pdf}.pdf"
+echo $watchFolder, $downloadFile >> /tmp/message
 
 #if the url does not start with http:// or https://, add it
-if [[ $POPCLIP_TEXT != http* ]] 
-    POPCLIP_TEXT="http://$POPCLIP_TEXT"
+target=$POPCLIP_TEXT
+if [[ $target != http* ]];then
+    target="http://$target"
 fi
+echo $target >> /tmp/message
 
 if [ -n $watchFolder ]; then
     if [ ! -d $watchFolder ]; then
        mkdir $watchFolder
     fi 
-    wget "$POPCLIP_TEXT" -O $watchFolder/$downloadFile
+    wget "$target" -O $watchFolder/$downloadFile
     exit $?
 else
     exit 1
